d4cde7e82499b8de15a1e92c1170f7f56cb82b34
권구성 <goosung.kwon@kurlycorp.com>
2023-07-29 20:38:53 +0900

    p37. Chapter 02 - VariableInitializationExample.java 예재 - 컴파일이 실패하지 않도록 변수 초기화 수정 로직 적용

--- temp/src/ch02/sec01/VariableInitializationExample.java
+++ temp/src/ch02/sec01/VariableInitializationExample.java
@@ -2,7 +2,7 @@ package ch02.sec01;
 
 public class VariableInitializationExample {
     public static void main(String[] args) {
-        int value;
+        int value = 0; // 컴파일이 실패하지 않도록 변수 초기화 수정 로직 적용
 
         // 연산 결과를 변수 result 에 할당하지만 value 가 초기화되지 않음
         int result = value + 10;
